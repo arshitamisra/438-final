9352175612e7cb0bf8b0962ef94980a409689135
arshitamisra <114960492+arshitamisra@users.noreply.github.com>
2024-11-22 15:01:37 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import RecipeDetails from './pages/RecipeDetails';
+
+const App = () => {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/recipe/:id" element={<RecipeDetails />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
+
